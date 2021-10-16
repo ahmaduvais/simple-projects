dae14ddb4987dcc5847318e52eed974ab565c36b
patanhii <92628769+patanhii@users.noreply.github.com>
2021-10-16 16:00:36 +0530

    Add files via upload

---
+++ practice1average_3numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+double average ( int a, int b, int c);
+int main(){
+    int a, b, c;
+    printf("Enter value of a\n");
+    scanf("%d",&a);
+    printf("Enter value of b\n");
+    scanf("%d",&b);
+    printf("Enter value of c\n");
+    scanf("%d",&c);
+    average(a, b, c);
+    return 0;
+}
+double average ( int a, int b, int c){
+    double result=(float)(a+b+c)/3;
+    printf("Average is %f\n", result);
+    return result;
+}
